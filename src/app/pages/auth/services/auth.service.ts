91c1e5541d18b180860cf754ac2db4bc0d4fe0f4
Diego Florez <andresflorezrincon11@gmail.com>
2025-09-05 11:14:14 -0500

    Video 38.2 login con el token

--- src/app/pages/auth/services/auth.service.ts
+++ src/app/pages/auth/services/auth.service.ts
@@ -1,19 +1,38 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Login } from '../models/login.interface';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { ApiResponse } from 'src/app/commons/response.interface';
-import { environment as env} from 'src/environments/environment';
+import { environment as env } from 'src/environments/environment';
 import { endpoint, httpOptions } from '@shared/apis/endpoints';
+import { map } from 'rxjs/operators';
+import { JsonPipe } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private user: BehaviorSubject<ApiResponse>;
+  public get userToken():ApiResponse{
+    return this.user.value;
+  }
 
-  constructor(private http: HttpClient) { }
-  login(req:Login): Observable<ApiResponse>{
-    const requestUrl =`${env.api}${endpoint.GENERATE_TOKEN}`
-    return this.http.post<ApiResponse>(requestUrl,req,httpOptions);
+  constructor(private http: HttpClient) {
+    this.user = new BehaviorSubject<ApiResponse>(
+      JSON.parse(localStorage.getItem("token"))
+    );
+
+  }
+  login(req: Login): Observable<ApiResponse> {
+    const requestUrl = `${env.api}${endpoint.GENERATE_TOKEN}`
+    return this.http.post<ApiResponse>(requestUrl, req, httpOptions).pipe(
+      map((resp: ApiResponse) => {
+        if (resp.isSuccess) {
+          localStorage.setItem("token", JSON.stringify(resp.data))
+          this.user.next(resp.data)
+        }
+        return resp;
+      })
+    )
   }
 }
